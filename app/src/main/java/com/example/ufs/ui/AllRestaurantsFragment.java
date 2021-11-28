f01aa1f08ac8c66309b7c67a549212956f54f5ee
Miguel <miguellealw@gmail.com>
2021-11-28 05:22:57 -0600

    Created a recycler view for all restaurants

--- app/src/main/java/com/example/ufs/ui/AllRestaurantsFragment.java
+++ app/src/main/java/com/example/ufs/ui/AllRestaurantsFragment.java
@@ -1,14 +1,22 @@
 package com.example.ufs.ui;
 
+import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
+import com.example.ufs.DatabaseHelper;
 import com.example.ufs.R;
+import com.example.ufs.data.model.RestaurantModel;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -16,6 +24,9 @@ import com.example.ufs.R;
  * create an instance of this fragment.
  */
 public class AllRestaurantsFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -61,6 +72,32 @@ public class AllRestaurantsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_all_restaurants, container, false);
+        View view =  inflater.inflate(R.layout.fragment_all_restaurants, container, false);
+        // Inflate the layout for this fragment
+        Context ctx = getActivity().getApplicationContext();
+
+        DatabaseHelper dbo = new DatabaseHelper(ctx);
+        List<RestaurantModel> restaurantList = dbo.getAllRestaurants();
+        TextView noRestaurantsMessage = view.findViewById(R.id.noRestaurantsStudentMessage);
+
+        // If there are restaurants
+        if(restaurantList != null) {
+            // Set up recycler view and display
+            recyclerView = view.findViewById(R.id.lv_restaurantList);
+            recyclerView.setVisibility(View.VISIBLE);
+            noRestaurantsMessage.setVisibility(View.GONE);
+            recyclerView.setHasFixedSize(true);
+
+            layoutManager = new LinearLayoutManager(ctx);
+            recyclerView.setLayoutManager(layoutManager);
+
+            //mAdapter = new MyAdapter(restaurantList);
+            mAdapter = new RestaurantRecyclerView(restaurantList, ctx);
+            recyclerView.setAdapter(mAdapter);
+        }
+
+
+
+        return view;
     }
 }
