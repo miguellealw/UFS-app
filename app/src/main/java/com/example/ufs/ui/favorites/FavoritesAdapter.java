a2801afda126f46c1a3aac48c51b87a6a4aaf5d1
Miguel <miguellealw@gmail.com>
2021-12-06 19:06:56 -0600

    User can favorite restaurant

--- app/src/main/java/com/example/ufs/ui/favorites/FavoritesAdapter.java
+++ app/src/main/java/com/example/ufs/ui/favorites/FavoritesAdapter.java
@@ -1,4 +1,86 @@
 package com.example.ufs.ui.favorites;
 
-public class FavoritesAdapter {
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.ufs.R;
+import com.example.ufs.data.model.FavoriteRestaurantModel;
+import com.example.ufs.data.model.RestaurantModel;
+import com.example.ufs.ui.restaurants.RestaurantAdapter;
+
+import java.util.List;
+
+public class FavoritesAdapter extends RecyclerView.Adapter<FavoritesAdapter.FavoriteViewHolder> {
+    List<FavoriteRestaurantModel> favoritesList;
+    Context context;
+    private FavoritesAdapter.OnItemClickListener listener;
+
+    public FavoritesAdapter(List<FavoriteRestaurantModel> favoritesList, Context context) {
+        this.favoritesList = favoritesList;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public FavoritesAdapter.FavoriteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Get layout
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.one_line_restaurant, parent, false);
+        // Associate with restaurant one line view holder
+        FavoritesAdapter.FavoriteViewHolder holder = new FavoritesAdapter.FavoriteViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull FavoritesAdapter.FavoriteViewHolder holder, int position) {
+        // Populate text views with list data
+        holder.tv_restaurantName.setText(favoritesList.get(position).getRestaurantName());
+        holder.tv_restaurantLocation.setText(favoritesList.get(position).getRestaurantLocation());
+        //int id = favoritesList.get(position).getId();
+
+        //holder.itemView.setTag(id);
+    }
+
+    @Override
+    public int getItemCount() {
+        return favoritesList.size();
+    }
+
+    // Reference to the one_line_restaurant view
+    public class FavoriteViewHolder extends RecyclerView.ViewHolder {
+        TextView tv_restaurantName;
+        TextView tv_restaurantLocation;
+
+        public FavoriteViewHolder(View itemView) {
+            super(itemView);
+            tv_restaurantName = itemView.findViewById(R.id.tv_list_restaurantName);
+            tv_restaurantLocation = itemView.findViewById(R.id.tv_list_restaurantLocation);
+
+            // When restaurant is clicked send the restaurant model to the event handler
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int pos = getAdapterPosition();
+                    if(listener != null && pos != RecyclerView.NO_POSITION) {
+                        listener.onItemClick(favoritesList.get(pos));
+                    }
+                }
+            });
+        }
+    }
+
+    public void setOnItemClickListener(FavoritesAdapter.OnItemClickListener listener) {
+        this.listener = listener;
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(FavoriteRestaurantModel favorite);
+    }
+
 }
--- app/src/main/java/com/example/ufs/ui/favorites/FavoritesFragment.java
+++ app/src/main/java/com/example/ufs/ui/favorites/FavoritesFragment.java
@@ -4,14 +4,26 @@ import android.content.Context;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.ufs.DatabaseHelper;
 import com.example.ufs.R;
 import com.example.ufs.SP_LocalStorage;
+import com.example.ufs.data.model.Cart;
+import com.example.ufs.data.model.FavoriteRestaurantModel;
+import com.example.ufs.data.model.RestaurantModel;
+import com.example.ufs.ui.restaurants.AllRestaurantsFragmentDirections;
+import com.example.ufs.ui.restaurants.RestaurantAdapter;
+
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -19,6 +31,9 @@ import com.example.ufs.SP_LocalStorage;
  * create an instance of this fragment.
  */
 public class FavoritesFragment extends Fragment {
+    private RecyclerView recyclerView;
+    private FavoritesAdapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -67,10 +82,50 @@ public class FavoritesFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_favorites, container, false);
         Context ctx = getActivity().getApplicationContext();
         DatabaseHelper dbo = new DatabaseHelper(ctx);
+        TextView noFavoritesMessage = view.findViewById(R.id.noFavoritesMessage);
+
+        Cart cart = Cart.getInstance();
+        cart.clearCart();
 
         SP_LocalStorage sp = new SP_LocalStorage(ctx);
         int userId = sp.getLoggedInUserId();
-        dbo.getAllUserFavoriteRestaurants(userId);
+        List<FavoriteRestaurantModel> favoritesList = dbo.getAllUserFavoriteRestaurants(userId);
+
+        if(favoritesList != null) {
+            // Set up recycler view and display
+            recyclerView = view.findViewById(R.id.rv_favorites);
+            recyclerView.setHasFixedSize(true);
+
+            // Show list and hide no restaurants message
+            recyclerView.setVisibility(View.VISIBLE);
+            noFavoritesMessage.setVisibility(View.GONE);
+
+            layoutManager = new LinearLayoutManager(ctx);
+            recyclerView.setLayoutManager(layoutManager);
+
+            // Add data to the recycler view
+            mAdapter = new FavoritesAdapter(favoritesList, ctx);
+            recyclerView.setAdapter(mAdapter);
+
+
+            mAdapter.setOnItemClickListener(new FavoritesAdapter.OnItemClickListener() {
+                @Override
+                public void onItemClick(FavoriteRestaurantModel favRestaurant) {
+                    // ACTION TO RESTAURANT INFO FRAGMENT
+                    FavoritesFragmentDirections.ActionFavoritesFragmentToRestaurantInfoFragment
+                            action = FavoritesFragmentDirections
+                            .actionFavoritesFragmentToRestaurantInfoFragment(
+                                    favRestaurant.getRestaurantId(),
+                                    favRestaurant.getRestaurantName(),
+                                    favRestaurant.getRestaurantLocation()
+                            );
+
+                    NavController navController = Navigation.findNavController(view);
+                    navController.navigate(action);
+
+                }
+            });
+        }
 
         return view;
     }
